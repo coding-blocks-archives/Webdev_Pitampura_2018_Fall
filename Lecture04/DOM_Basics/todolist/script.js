e87620d94226ebe8e22e24f7a20dc7bea024cd5a
Arnav Gupta <arnav@codingblocks.com>
2018-09-01 19:12:11 +0530

    fizz buzz skeleton
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture04/DOM_Basics/todolist/script.js
+++ Lecture04/DOM_Basics/todolist/script.js
@@ -3,5 +3,10 @@ let btnAddTask = document.getElementById('addtask')
 let ulTodoList = document.getElementById('todolist')
 
 btnAddTask.onclick = function () {
+    let start  = new Date().getTime()
     ulTodoList.innerHTML += "<li>" + inpNewtask.value + "</li>"
+
+    // for loop etc..
+
+    console.log(new Date().getTime() - start)
 }
