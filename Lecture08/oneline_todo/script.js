898c860b3e392b9c0113465a458946c364da22e9
Arnav Gupta <arnav@codingblocks.com>
2018-09-15 17:32:29 +0530

    single function todolist
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture08/oneline_todo/script.js
@@ -0,0 +1,42 @@
+$(() => 
+    $('body')
+    .append($('<input>').attr('id', 'newtask'))
+    .append(
+      $('<button>')
+        .text('ADD')
+        .click(() => 
+          $('#tasklist').append(
+            $('<li>')
+            .append(
+                $('<button>')
+                    .text('X')
+                    .click((e) => $(e.target).parent().remove())
+            )
+            .append(
+              $('<button>')
+                .attr('class', 'down-btn')
+                .text('⬇️')
+                .click((e) => 
+                  $(e.target).parent()
+                    .insertAfter($(e.target).parent().next())
+                )
+            )
+            .append(
+              $('<button>')
+                .attr('class', 'up-btn')
+                .text('⬆️')
+                .click((e) => 
+                  $(e.target).parent()
+                    .insertBefore($(e.target).parent().prev())
+                )
+            )
+            .append($('<span>').text($('#newtask').val()))
+          )
+        )
+    )
+    .append(
+      $('<ul>')
+        .attr('id', 'tasklist')
+    )
+)
+                
