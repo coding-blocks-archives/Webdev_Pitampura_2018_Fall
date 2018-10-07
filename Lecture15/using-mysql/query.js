97b0ec5f59e59019b4bdeba66ec99c22a82f597e
Arnav Gupta <arnav@codingblocks.com>
2018-10-07 18:22:33 +0530

    oneline
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture15/using-mysql/query.js
+++ Lecture15/using-mysql/query.js
@@ -8,6 +8,21 @@ async function performquery () {
   })
   const [rows, cols] = await conn.query('select * from tasks')
   console.log(JSON.stringify(rows, null, 2))
+  // let colHeader = ''
+  // for (let col of cols) {
+  //   colHeader += col.name + '\t'
+  // }
+
+  console.log(cols.reduce((a, c) => a + c.name + '\t', ''))
+
+  rows.forEach(r => console.log(cols.map(c => r[c.name]).reduce((a, i) => a + i + '\t', '')))
+
+  // for (let row of rows) {
+  //   console.log(
+  //     row[cols[0].name] + '\t' +
+  //     row[cols[1].name] + '\t' +
+  //     row[cols[2].name])
+  // }
   conn.close()
 }
 
