8aba98eefc52a830438986b7d98f388cda0398b0
Arnav Gupta <arnav@codingblocks.com>
2018-10-28 18:44:45 +0530

    login with session
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture19/user-auth/routes/login.js
+++ Lecture19/user-auth/routes/login.js
@@ -1,8 +1,36 @@
 const route = require('express').Router()
+const { User } = require('../db')
 
 route.get('/', (req, res) => {
- res.render('login')
+  console.log('GET /login')
+  res.render('login')
 })
 
+route.post('/', async (req, res) => {
+  console.log('POST /login')
+  try {
+    const user = await User.findOne({
+      where: { username: req.body.username }
+    })
+    if (!user) {
+      // username wrong
+      console.log('username wrong')
+      return res.redirect('/login')
+    }
 
- module.exports = route
+    if (user.password !== req.body.password) {
+      // password is wrong
+      console.log('password wrong')
+      return res.redirect('/login')
+    }
+    console.log('going to profile')
+    req.session.userId = user.id
+    res.redirect('/profile')
+
+  } catch (e) {
+    console.log('going back to login')
+    res.redirect('/login')
+  }
+})
+
+module.exports = route
--- Lecture19/user-auth/routes/profile.js
+++ Lecture19/user-auth/routes/profile.js
@@ -1,4 +1,24 @@
 const route = require('express').Router()
+const { User } = require('../db')
+
+route.get('/', async (req, res) => {
+
+  if (req.session && req.session.userId) {
+    console.log('session exists')
+    const user = await User.findOne({
+      attributes: [ 'id', 'username', 'email' ],
+      where: {
+        id: req.session.userId
+      }
+    })
+
+    res.render('profile', { user })
+
+  } else {
+    console.log('session does not exist')
+    res.redirect('/login')
+  }
+})
 
 
- module.exports = route
+module.exports = route
